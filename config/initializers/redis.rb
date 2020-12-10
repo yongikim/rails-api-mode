6f995dae7dec98a3f63750752438b232bb4a3ce4
yongikim <yongi.kim08@gmail.com>
2020-12-10 23:56:24 +0900

    Fix typo

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
 REDIS = if Rails.env.test?
           MockRedis.new
-        else if Redis.env.production?
+        elsif Rails.env.production?
           Redis.new(url: ENV['REDIS_URL'])
         else
           Redis.new(url: Settings.session.url)
