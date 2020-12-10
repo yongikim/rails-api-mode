8f44b1e1ec26a92a9052783e63c8eeaadce16f00
yongikim <yongi.kim08@gmail.com>
2020-12-10 23:52:25 +0900

    Get redis url from env on production

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,7 @@
 REDIS = if Rails.env.test?
           MockRedis.new
+        else if Redis.env.production?
+          Redis.new(url: ENV['REDIS_URL'])
         else
           Redis.new(url: Settings.session.url)
         end
