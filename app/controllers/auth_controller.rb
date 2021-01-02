78d71656320cfa93513986e94d79bde712398156
Yongi Kim <yongikim@Yongis-MacBook-Pro.local>
2021-01-03 02:12:58 +0900

    Add an endpoint for checking auth state

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,13 +4,21 @@ class AuthController < ApplicationController
   # POST /auth
   def create
     user = User.find_by(email: params[:email])
-    token = ''
     status = :unauthorized
     if user&.authenticate(params[:password])
       token = Session.create(user)
       status = :created
       cookies['token'] = { value: token, httponly: true }
     end
-    render json: { token: token }, status: status
+    render status: status
+  end
+
+  # GET /auth/user_id
+  def user_id
+    token = cookies['token']
+    session = REDIS.hgetall(token)
+    status = session['user_id'].present? ? :ok : :unauthorized
+
+    render json: { user_id: session['user_id'] }, status: status
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :users
   resources :auth, only: %i[create destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'auth/user_id' => 'auth#user_id'
 end
